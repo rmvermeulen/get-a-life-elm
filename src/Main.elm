0a7f911fefb54e54dfc38446de845f45b098658b
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-30 15:53:27 +0200

    Add some colors

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (..)
 
 import Browser
+import Colors.Opaque as Colors
 import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
 
 
 
@@ -36,7 +41,20 @@ update msg model =
 
 view : Model -> Element Msg
 view model =
-    column [] []
+    column
+        [ Background.color Colors.white
+        , padding 10
+        , Border.width 1
+        , Border.shadow
+            { blur = 4
+            , color = Colors.black
+            , offset = ( 2, 1 )
+            , size = 1
+            }
+        ]
+        [ text "App is running!"
+        , text <| "model: " ++ Debug.toString model
+        ]
 
 
 
@@ -46,7 +64,13 @@ view model =
 main : Program () Model Msg
 main =
     Browser.element
-        { view = view >> layout []
+        { view =
+            view
+                >> layout
+                    [ padding 10
+                    , Border.solid
+                    , Background.color Colors.aliceblue
+                    ]
         , init = \_ -> init
         , update = update
         , subscriptions = always Sub.none
