3fbdf31945f151fe43fa54b663c48889f26367bd
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-04-05 17:59:08 +0200

    Add a test for Profile.toString

--- src/Types/Profile.elm
+++ src/Types/Profile.elm
@@ -1,4 +1,4 @@
-module Types.Profile exposing (Profile(..))
+module Types.Profile exposing (Profile(..), toString)
 
 import Types.Birth as Birth exposing (Birth)
 import Types.BodyInfo as BodyInfo exposing (BodyInfo)
@@ -16,3 +16,47 @@ type Profile
         , bodyInfo : BodyInfo
         , class : Class
         }
+
+
+toString : Profile -> String
+toString profile =
+    let
+        m2s s f =
+            Maybe.map (f >> (++) s)
+
+        bs mBirth =
+            m2s "Birth=" Birth.toString mBirth
+
+        bis mBodyInfo =
+            m2s "BodyInfo=" BodyInfo.toString mBodyInfo
+
+        cs mClass =
+            m2s "Class=" Class.toString mClass
+
+        summary ms =
+            ms
+                |> List.filterMap identity
+                |> String.join " "
+    in
+    case profile of
+        Partial { mBirth, mBodyInfo, mClass } ->
+            let
+                content =
+                    summary
+                        [ bs mBirth
+                        , bis mBodyInfo
+                        , cs mClass
+                        ]
+            in
+            "[Profile:Partial {" ++ content ++ "}]"
+
+        Complete { birth, bodyInfo, class } ->
+            let
+                content =
+                    summary
+                        [ bs <| Just birth
+                        , bis <| Just bodyInfo
+                        , cs <| Just class
+                        ]
+            in
+            "[Profile:Complete {" ++ content ++ "}]"
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,7 +1,13 @@
 module Tests exposing (..)
 
-import Test exposing (..)
 import Expect
+import Test exposing (..)
+import Types.Birth exposing (Birth)
+import Types.BodyInfo as BodyInfo
+import Types.Class as Class exposing (Class(..))
+import Types.Place as Place exposing (Place(..))
+import Types.Profile as Profile exposing (Profile(..))
+
 
 
 -- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
@@ -13,10 +19,97 @@ all =
         [ test "Addition" <|
             \_ ->
                 Expect.equal 10 (3 + 7)
-        , test "String.left" <|
-            \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
+        ]
+
+
+types : Test
+types =
+    describe "toString methods of data types"
+        [ describe "Types.Profile"
+            [ test "toString: empty partial" <|
+                \_ ->
+                    let
+                        p =
+                            Profile.Partial
+                                { mBirth = Nothing
+                                , mBodyInfo = Nothing
+                                , mClass = Nothing
+                                }
+
+                        result =
+                            Profile.toString p
+
+                        expected =
+                            "[Profile:Partial {}]"
+                    in
+                    Expect.equal result expected
+            , test "toString: partial with class" <|
+                \_ ->
+                    let
+                        p =
+                            Profile.Partial
+                                { mBirth = Nothing
+                                , mBodyInfo = Nothing
+                                , mClass = Just Class.Lower
+                                }
+
+                        result =
+                            Profile.toString p
+
+                        expected =
+                            "[Profile:Partial {Class=Lower}]"
+                    in
+                    Expect.equal result expected
+            , test "toString: partial with birth, class" <|
+                \_ ->
+                    let
+                        p =
+                            Profile.Partial
+                                { mBirth = Just <| Birth Place.Afrika 2000
+                                , mBodyInfo = Nothing
+                                , mClass = Just Class.Lower
+                                }
+
+                        result =
+                            Profile.toString p
+
+                        expected =
+                            "[Profile:Partial {Birth=Afrika, 2000 Class=Lower}]"
+                    in
+                    Expect.equal result expected
+            , test "toString: partial with everything" <|
+                \_ ->
+                    let
+                        p =
+                            Profile.Partial
+                                { mBirth = Just <| Birth Place.Afrika 2000
+                                , mBodyInfo = Just <| BodyInfo.White
+                                , mClass = Just Class.Lower
+                                }
+
+                        result =
+                            Profile.toString p
+
+                        expected =
+                            "[Profile:Partial {Birth=Afrika, 2000 BodyInfo=White Class=Lower}]"
+                    in
+                    Expect.equal result expected
+            , test "toString: Complete" <|
+                \_ ->
+                    let
+                        p =
+                            Profile.Complete
+                                { birth = Birth Place.Australia 2000
+                                , bodyInfo = BodyInfo.Black
+                                , class = Class.Lower
+                                }
+
+                        result =
+                            Profile.toString p
+
+                        expected =
+                            "[Profile:Complete {Birth=Australia, 2000 BodyInfo=Black Class=Lower}]"
+                    in
+                    Expect.equal result expected
+            ]
         ]
